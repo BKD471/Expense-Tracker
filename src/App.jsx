944a99cb2d351f3b6f156f40866acc39a2490234
BKD471 <bhaskarkumardas9@gmail.com>
2023-04-13 19:02:37 +0530

    UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Expense from "./component/Expenses/Expenses";
+import NewExpense from "./component/NewExpense/NewExpense";
 
 const App = () => {
   return (
     <>
+      <NewExpense />
       <Expense />
     </>
   );
--- src/component/Expenses/ExpenseItem.jsx
+++ src/component/Expenses/ExpenseItem.jsx
@@ -1,15 +1,21 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, amount, date }) => {
+  const [text, setText] = useState(title);
+
+  const clickHandler = () => {};
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{text}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={() => clickHandler()}>Click</button>
     </Card>
   );
 };
